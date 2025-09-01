c1fe498a0dabc16110ecb065e9dd81c647fc835b
Pedro Pinas <joaopedrolp2204@gmail.com>
2025-09-01 13:37:27 -0300

    refactor: rename tmp folder to database and move migrations inside

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,14 @@
-import { knex as setupKnex } from "knex";
+import { knex as setupKnex, Knex } from "knex";
 
-export const config = {
+export const config: Knex.Config = {
     client: 'sqlite3',
     connection: {
-        filename: './tmp.db'
+        filename: './app.db'
     },
     useNullAsDefault: true,
+    migrations:{
+        extension: 'ts',
+        directory: './database/migrations'
+    }
 }
 export const knex = setupKnex(config)
