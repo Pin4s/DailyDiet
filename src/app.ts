8912e2cbefe53ee7b32b2f6b333d46cf6cc8c792
Pedro Pinas <joaopedrolp2204@gmail.com>
2025-09-02 13:28:59 -0300

    feat: add auth routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify'
 import { userRoutes } from './routes/user-route'
+import { authRoutes } from './routes/auth-routes'
 
 export const app = fastify()
 
 app.register(userRoutes, {prefix: 'users'})
+app.register(authRoutes, {prefix: 'auth'})
